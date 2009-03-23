aa05ccd29c1f92f9f75e1bfe2f8039a872bc955a
Dirk Breuer <dirk.breuer@gmail.com>
2009-03-24 00:17:44 +0100

    CHANGED - added README with an example. Added some simple test cases

--- test/test_config_fu.rb
+++ test/test_config_fu.rb
@@ -3,9 +3,28 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestConfigFu < Test::Unit::TestCase
 
   def setup
+    Rails.stubs(:env => 'development')
   end
   
-  def test_truth
-    assert true
+  def test_should_add_config_methods_dynamically
+    ConfigFu::Configuration.configure { |config| config.mailer = "MockMailer" }
+    assert_equal "MockMailer", ConfigFu.config.mailer
   end
+  
+  def test_should_respect_environment_specific_config_before_the_general_one
+    ConfigFu::Configuration.configure { |config| config.mailer = "MockMailer" }
+    assert_equal "MockMailer", ConfigFu.config.mailer
+    
+    ConfigFu::Configuration.configure(:development) { |config| config.mailer = "DevelopmentMailer" }
+    assert_equal "DevelopmentMailer", ConfigFu.config.mailer
+  end
+  
+  def test_should_ignore_config_if_the_current_environment_doesnt_matches_the_given_one
+    ConfigFu::Configuration.configure { |config| config.mailer = "MockMailer" }
+    assert_equal "MockMailer", ConfigFu.config.mailer
+    
+    ConfigFu::Configuration.configure(:production) { |config| config.mailer = "LiveMailer" }
+    assert_equal "MockMailer", ConfigFu.config.mailer
+  end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,8 @@
-require 'stringio'
+require 'rubygems'
+
 require 'test/unit'
+require 'mocha'
+
 require File.dirname(__FILE__) + '/../lib/config_fu'
+
+class Rails; end
